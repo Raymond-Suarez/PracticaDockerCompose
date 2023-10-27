1af3a18fb2238558706e0b34e0e4feb9843bf841
Raymond Salvador Suarez Levasseur <raymondsuarez1407@gmail.com>
2023-10-27 18:19:05 -0400

    Update index.php

--- docker-php-mysql/index.php
+++ docker-php-mysql/index.php
@@ -4,16 +4,15 @@
     <title>Página de Presentación</title>
 </head>
 <body>
-    <h1>Bienvenido a mi página de presentación en PHP</h1>
-    <p>Esta es una página de ejemplo creada con PHP.</p>
+    <h1>Esto es mi pagina funcionando con Docker Compose</h1>
+    <p>Se utilizo php y mysql.</p>
 
     <?php
-    // Puedes insertar código PHP en medio del HTML
     $nombre = "Raymond Suarez";
     $matricula = "2020-10427";
     echo "<p>Mi nombre es $nombre.</p>";
     echo "<p>Mi matricula es $matricula.</p>";
     ?>
-    
+
 </body>
 </html>
